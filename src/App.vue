48c59da55a0ffb74da8f65a7c3fbc13f432e6218
Olli P <ollip@outlook.com>
2024-10-14 10:59:27 +0300

    first commit all new

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const textstring = 'Hello World'
+const message = ref('')
+</script>
+
+<template>
+  <div>
+    <textarea v-model="message" :placeholder="textstring" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
